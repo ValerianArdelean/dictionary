b1a1a8396d43105432d778507cee9e97613cc5b7
ValerianArdelean <valerian_ardelean@yahoo.com>
2024-02-26 00:28:34 +0200

    Refine
    I refined the style for the information displayed
    I also added quotation marks around the user data displayed.

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ document.getElementById("searchBttn").addEventListener("click", function(event) 
     let searchedWord = event.srcElement.form[1].value;
     let searchResult = words.includes(searchedWord);
     let p = document.createElement("p");
-    p.innerText = `The searched word: ${searchedWord} is NOT present in our list !`;
+    p.innerText = `The searched word: "${searchedWord}" is NOT present in our list !`;
     if (searchResult) {
-        p.innerText = `The searched word: ${searchedWord}  it IS present in our list ! `;
+        p.innerText = `The searched word: "${searchedWord}" it IS present in our list ! `;
     }
     document.getElementById("left").appendChild(p);
 });
