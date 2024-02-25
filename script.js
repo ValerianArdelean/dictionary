603b7c68d54f0fc6cfe616b6555a2400e971f1d4
ValerianArdelean <valerian_ardelean@yahoo.com>
2024-02-26 00:23:23 +0200

    Implemented the adding word feature

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let words = ["fara", "cuvinte"];
+let words = [];
+
 document.getElementById("searchBttn").addEventListener("click", function(event) {
     event.preventDefault();
     let searchedWord = event.srcElement.form[1].value;
@@ -10,3 +11,15 @@ document.getElementById("searchBttn").addEventListener("click", function(event) 
     }
     document.getElementById("left").appendChild(p);
 });
+
+document.getElementById("insertBttn").addEventListener("click", function(event2) {
+    event2.preventDefault();
+    let insertededWord = event2.srcElement.form[4].value;
+    let o = words.push(insertededWord);
+    let p = document.createElement("p");
+    p.innerText = "success";
+    let parrent = document.getElementById("rigth");
+    parrent.appendChild(p)
+});
+
+
