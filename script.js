bd954f33220d063493f5f5bba09247e72d281103
ValerianArdelean <valerian_ardelean@yahoo.com>
2025-01-31 23:40:58 +0200

    optimising the code

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let words = [];
-let n = parseInt(words.length);
 
 function searchWord() {
     let p = document.createElement("p");
@@ -14,16 +13,10 @@ function searchWord() {
 function insertWord() {
     let p = document.createElement("p");
 	p.innerText = "Failed !";
-    let insertededWord = document.getElementById("word");
-    if (insertededWord.value != "") {
-        let o = words.push(insertededWord.value);
-        if (parseInt(o) === n + 1) {
-            ++n;
-            p.innerText = `The word "${insertededWord.value}" has been successfully inserted!`;
-        }
+    let insertedWord = document.getElementById("word");
+    if (insertedWord.value != "") {
+        words.push(insertedWord.value);
+		p.innerText = `The word "${insertedWord.value}" has been successfully inserted!`;
     }
     document.getElementById("insertResult").appendChild(p);
 };
-
-
-
